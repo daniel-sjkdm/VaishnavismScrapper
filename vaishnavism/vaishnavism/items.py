91acf7f66f2eb0104446381abae9e845bfed9b63
daniel-sjkdm <daniel.py.216@gmail.com>
2020-09-22 11:16:40 -0500

    ekadasi and iskcon items

---
+++ vaishnavism/vaishnavism/items.py
@@ -0,0 +1,18 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class EkadasiItem(scrapy.Item):
+    name = scrapy.Field()
+    date = scrapy.Field()
+    start = scrapy.Field()
+    end = scrapy.Field()
+
+
+class IskconEventItem(scrapy.Item):
+    name = scrapy.Field()
+    date = scrapy.Field()
