a7f1f50f0dcb5fbc113c0f4c05da857120a7998f
daniel-sjkdm <daniel.py.216@gmail.com>
2020-09-22 17:35:23 -0500

    Connect to sqlite3 and add items to db

--- vaishnavism/vaishnavism/pipelines.py
+++ vaishnavism/vaishnavism/pipelines.py
@@ -6,8 +6,26 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from .database import Vaishnadb 
 
 
 class VaishnavismPipeline:
+    
+    db = Vaishnadb()
+
     def process_item(self, item, spider):
-        return item
+        if spider.name == "ekadasi_spider":
+            name = item["name"]
+            day = item["day"]
+            month = item["month"]
+            year = item["year"]
+            start = item["start"][0]
+            end = item["start"][0]
+            self.db.addEkadasiItem(name, day, month, year, start, end)        
+
+        elif spider.name == "iskcon_calendar":
+            name = item["name"]
+            month = item["month"]
+            day = item["day"]
+            year = item["year"]
+            self.db.addIskconEventItem(name, month, day, year)
