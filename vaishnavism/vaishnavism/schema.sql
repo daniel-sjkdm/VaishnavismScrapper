ed6b992f55972cff2ee9795ebcc171c4585b260f
daniel-sjkdm <daniel.py.216@gmail.com>
2020-09-22 17:38:53 -0500

    Basic db schema for testing

---
+++ vaishnavism/vaishnavism/schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE ekadasi_dates (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+name VARCHAR(100),
+day VARCHAR(10),
+month VARCHAR(10),
+year VARCHAR(10),
+start VARCHAR(5),
+end VARCHAR(5));
+CREATE TABLE sqlite_sequence(name,seq);
+CREATE TABLE iskcon_events (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+name VARCHAR(100),
+month VARCHAR(10),
+day VARCHAR(10),
+year VARCHAR(4)
+);
