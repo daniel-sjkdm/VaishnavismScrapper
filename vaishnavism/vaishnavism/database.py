1e685673aff8def9f01b87d861bcc76b28943323
daniel-sjkdm <daniel.py.216@gmail.com>
2020-10-06 14:04:42 -0500

    Connection class for the Heroku PG database

--- vaishnavism/vaishnavism/database.py
+++ vaishnavism/vaishnavism/database.py
@@ -1,4 +1,7 @@
+import os
+from dotenv import load_dotenv
 import sqlite3
+import psycopg2
 
 
 class Vaishnadb:
@@ -21,3 +24,34 @@ class Vaishnadb:
             (name, month, day, year)
             VALUES (?, ?, ?, ?)""", (name, month, day, year))
         self.conn.commit()
+
+
+
+class VaishnadbPG:
+    load_dotenv()
+    def __init__(self):
+        self.conn = psycopg2.connect(
+            dbname = os.getenv('PGDATABASE'),
+            host = os.getenv('PGHOST'),
+            port = os.getenv('PGPORT'),
+            user = os.getenv('PGUSER'),
+            password = os.getenv('PGPASSWORD')
+        )
+        self.cur = self.conn.cursor()
+
+    def addEkadasiItem(self, name, day, month, year, starts, ends):
+        self.cur.execute("""
+            INSERT INTO ekadasi_events  
+            (name, day, month, year, starts, ends)
+            VALUES (%s, %s, %s, %s, %s, %s)""", (name, day, month, year, starts, ends))
+        self.conn.commit()
+        
+             
+    def addIskconEventItem(self, name, month, day, year):
+        self.cur.execute("""
+            INSERT INTO iskcon_events 
+            (name, month, day, year)
+            VALUES (%s, %s, %s, %s)""", (name, month, day, year))
+        self.conn.commit()
+
+
