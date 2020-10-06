ddac4daf4bf2e716f6ec42a9ca4c0c4617415cac
daniel-sjkdm <daniel.py.216@gmail.com>
2020-10-06 14:06:04 -0500

    Pipeline to insert elements into the Heroku db

--- vaishnavism/vaishnavism/pipelines.py
+++ vaishnavism/vaishnavism/pipelines.py
@@ -5,8 +5,10 @@
 
 
 # useful for handling different item types with a single interface
+import os
 from itemadapter import ItemAdapter
 from .database import Vaishnadb 
+from .database import VaishnadbPG
 
 
 class VaishnavismPipeline:
@@ -19,8 +21,8 @@ class VaishnavismPipeline:
             day = item["day"]
             month = item["month"]
             year = item["year"]
-            start = item["start"][0]
-            end = item["start"][0]
+            start = item["starts"][0]
+            end = item["starts"][0]
             self.db.addEkadasiItem(name, day, month, year, start, end)        
 
         elif spider.name == "iskcon_calendar":
@@ -29,3 +31,29 @@ class VaishnavismPipeline:
             day = item["day"]
             year = item["year"]
             self.db.addIskconEventItem(name, month, day, year)
+
+
+
+class VaishnavismPipelinePG:
+
+    db = VaishnadbPG()
+
+    def process_item(self, item, spider):
+        if spider.name == "ekadasi_spider":
+            name = item["name"]
+            day = item["day"]
+            month = item["month"]
+            year = item["year"]
+            starts = item["starts"][0]
+            ends = item["starts"][0]
+            self.db.addEkadasiItem(name, day, month, year, starts, ends)        
+
+        elif spider.name == "iskcon_calendar":
+            name = item["name"]
+            month = item["month"]
+            day = item["day"]
+            year = item["year"]
+            self.db.addIskconEventItem(name, month, day, year)
+
+
+
