c4b9d269ca9c66ec5ac86555d63b245189f20d19
daniel-sjkdm <daniel.py.216@gmail.com>
2020-10-06 00:11:09 -0500

    The year field was changed to char size 4

--- vaishnavism/vaishnavism/schema.sql
+++ vaishnavism/vaishnavism/schema.sql
@@ -1,16 +1,16 @@
 CREATE TABLE ekadasi_dates (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-name VARCHAR(100),
-day VARCHAR(10),
-month VARCHAR(10),
-year VARCHAR(10),
-start VARCHAR(5),
-end VARCHAR(5));
-CREATE TABLE sqlite_sequence(name,seq);
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(100),
+    day VARCHAR(10),
+    month VARCHAR(10),
+    year VARCHAR(4),
+    start VARCHAR(5),
+    end VARCHAR(5));
+
 CREATE TABLE iskcon_events (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-name VARCHAR(100),
-month VARCHAR(10),
-day VARCHAR(10),
-year VARCHAR(4)
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(100),
+    month VARCHAR(10),
+    day VARCHAR(10),
+    year VARCHAR(4)
 );
