28054c9b39a6b29340cc94d06a447e73f2afb8b3
daniel-sjkdm <daniel.py.216@gmail.com>
2020-10-09 14:32:39 -0500

    Day string 2 number

--- vaishnavism/vaishnavism/spiders/helpers.py
+++ vaishnavism/vaishnavism/spiders/helpers.py
@@ -1,3 +1,13 @@
+DAY_2_NUMBER = {
+    "Monday": "1",
+    "Tuesday": "2",
+    "Wednesday": "3",
+    "Thursday": "4",
+    "Friday": "5",
+    "Saturday": "6",
+    "Sunday": "7"
+}
+
 MONTH_2_NUMBER = {
     "January": "1",
     "February": "2",
