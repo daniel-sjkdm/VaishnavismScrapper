5be195cfd9521e779f8379f7d914e09fb8082433
daniel-sjkdm <daniel.py.216@gmail.com>
2020-10-09 16:53:28 -0500

    PIpeline changed to met the database schema

--- vaishnavism/vaishnavism/pipelines.py
+++ vaishnavism/vaishnavism/pipelines.py
@@ -1,59 +1,41 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
 import os
 from itemadapter import ItemAdapter
-from .database import Vaishnadb 
-from .database import VaishnadbPG
+from .database import Vaishnadb
+from .settings import DEBUG
+
+if DEBUG:
+    from dotenv import load_dotenv
+    load_dotenv()
 
 
 class VaishnavismPipeline:
     
-    db = Vaishnadb()
+    dbname = os.getenv('PGDATABASE', None)
+    host = os.getenv('PGHOST', None)
+    port = os.getenv('PGPORT', None)
+    user = os.getenv('PGUSER', None)
+    password = os.getenv('PGPASSWORD', None)    
+
+    db = Vaishnadb(dbname, host, port, user, password)
+
 
     def process_item(self, item, spider):
+
         if spider.name == "ekadasi_spider":
+
             name = item["name"]
-            day = item["day"]
-            month = item["month"]
-            year = item["year"]
-            start = item["starts"][0]
-            end = item["starts"][0]
-            self.db.addEkadasiItem(name, day, month, year, start, end)        
+            description = item["description"]
+            country = item["country"]
+            event_date = item["event_date"]
+            starts = item["starts"]
+            ends = item["ends"]
+            
+            ekadasi_id = self.db.add_ekadasi(name, description, return_id=True)[0]
+            self.db.add_ekadasi_date(ekadasi_id, country, event_date, starts, ends)
 
-        elif spider.name == "iskcon_calendar":
+        elif spider.name == "iskcon_events_spider":
+
             name = item["name"]
-            month = item["month"]
-            day = item["day"]
-            year = item["year"]
-            self.db.addIskconEventItem(name, month, day, year)
-
-
-
-class VaishnavismPipelinePG:
-
-    db = VaishnadbPG()
-
-    def process_item(self, item, spider):
-        if spider.name == "ekadasi_spider":
-            name = item["name"]
-            day = item["day"]
-            month = item["month"]
-            year = item["year"]
-            starts = item["starts"][0]
-            ends = item["starts"][0]
-            self.db.addEkadasiItem(name, day, month, year, starts, ends)        
-
-        elif spider.name == "iskcon_calendar":
-            name = item["name"]
-            month = item["month"]
-            day = item["day"]
-            year = item["year"]
-            self.db.addIskconEventItem(name, month, day, year)
-
-
-
+            description = item["description"]
+            event_date = item["event_date"]
+            self.db.add_iskcon_event(name, description, event_date)
